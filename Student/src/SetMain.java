27a121a56981c1178f54cb237061ca202ed6bbd4
lucifer <lucifer@DESKTOP-11SDPPU>
2020-08-07 11:15:14 +0530

    trying

---
+++ Student/src/SetMain.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class SetMain {
+
+    public static void main(String[] args) {
+    	SetMain demo = new SetMain();
+    	demo.runApp();
+    }
+    
+    public void runApp() {
+
+        Set<Student> set = new LinkedHashSet<>();
+
+        Student student1 = new Student(12, "ank123");
+        set.add(student1);
+        Student student2 = new Student(134, "antila123");
+        set.add(student2);
+        Student student3 = new Student(13, "ankur123");
+        set.add(student3);
+        Student student4 = new Student(12, "a123");
+        set.add(student4);
+        
+       
+        Iterator<Student> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            
+            Student student = iterator.next();
+            
+            System.out.println("element =" + student.getAge() + " " + student.getRollno());
+        }
+        
+       
+       
+            
+        }
+
+
+    }
---
+++ Student/src/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+	
+	private int age;
+	private String rollno;
+	
+	Student(int age, String rollno){
+		
+
+	this.age=age;
+	this.rollno= rollno;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(String rollno) {
+		this.rollno = rollno;
+	}
+
+}
